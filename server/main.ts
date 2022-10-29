ed55ca37a9b564de26114448fa9f3b3d969d711d
Adam Zerner <adamzerner@protonmail.com>
2022-10-29 16:01:51 -0700

    Set up dotenv

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import register from "@react-ssr/express/register";
+import dotenv from "dotenv";
 import pageRouter from "./pages";
 import apiRouter from "./api";
 
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 (async () => {
   await register(app);
@@ -12,6 +14,6 @@ const port = process.env.PORT || 3000;
   app.use("/api", apiRouter);
   app.use("/", pageRouter);
   app.listen(port, () => {
-    console.log("> Ready on http://localhost:3000");
+    console.log(`> Ready on http://localhost:${port}`);
   });
 })();
