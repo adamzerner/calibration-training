38416a399bfef7cf61832300fd7d37bf6c20a252
Adam Zerner <adamzerner@protonmail.com>
2022-10-29 13:34:06 -0700

    Cache static files

--- ssr.config.js
+++ ssr.config.js
@@ -1,4 +1,13 @@
 /* eslint-disable no-undef */
 module.exports = {
   distDir: "dist/.ssr",
+  staticViews: [
+    "auth/log-in",
+    "auth/sign-up",
+    "email",
+    "errors/404",
+    "help",
+    "home",
+    "sms",
+  ],
 };
